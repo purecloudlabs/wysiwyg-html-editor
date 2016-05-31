a2e9ec849c825bc440c5ded458afa53b1564a733
Ben Nelson <Ben.Nelson@inin.com>
2016-05-31 11:46:53 -0400

    [NO-JIRA] Add a getHTML method to the editor

--- src/Editor.js
+++ src/Editor.js
@@ -2,10 +2,32 @@
 
 var Quill = require("quill");
 
-function Editor(target, options) {
-    var quill = new Quill(target, options);
+/**
+ *  Constructs an Editor (a thin wrapper around QuillJS)
+ *  Should use the EditorBuilder to construct one of these
+ *  @constructor
+ *  @param {HTMLElement} targetEl - the DOM node that will be converted into the WYSIWYG editor
+ *  @param {Object} options - the options provided to QuillJS
+ */
 
+function Editor(targetEl, options) {
+    var quill = new Quill(targetEl, options);
+
+    this.targetEl = targetEl;
     this._quill = quill;
 }
+
+/**
+ * Returns the contents of the editor as html
+ * @returns {String}
+ *
+ */
+Editor.prototype.getHTML = function() {
+    var editorContentDiv = this.targetEl.querySelector(".ql-editor");
+    if(!editorContentDiv) {
+        throw new Error("Couldn't find editor contents");
+    }
+    return editorContentDiv.innerHTML;
+};
 
 module.exports = Editor;
