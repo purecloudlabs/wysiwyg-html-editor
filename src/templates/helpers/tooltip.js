603a1f8b711d38171ed887d5a936c6ba373fb91b
Ben Nelson <Ben.Nelson@inin.com>
2016-06-22 18:15:37 -0400

    [NO-JIRA] Add data-tooltip-class attr

--- src/templates/helpers/tooltip.js
+++ src/templates/helpers/tooltip.js
@@ -3,5 +3,5 @@ var Handlebars = require("handlebars");
 var i18n = require("../../i18n/i18n");
 module.exports = function (tooltipKey) {
     var safeText = Handlebars.Utils.escapeExpression(i18n.localize(tooltipKey));
-    return new Handlebars.SafeString('data-toggle="tooltip" data-placement="top" title="' + safeText + '"');
+    return new Handlebars.SafeString('data-toggle="tooltip" data-placement="top" data-tooltip-class="tt" title="' + safeText + '"');
 };
