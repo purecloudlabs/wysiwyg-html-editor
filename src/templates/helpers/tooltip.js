abb34dd9d36e704daccc80605259013178543361
Ben Nelson <Ben.Nelson@inin.com>
2016-09-01 16:04:28 -0400

    [NO-JIRA] var -> let or const

--- src/templates/helpers/tooltip.js
+++ src/templates/helpers/tooltip.js
@@ -1,7 +1,7 @@
 "use strict";
-var Handlebars = require("handlebars");
-var i18n = require("../../i18n/i18n");
+const Handlebars = require("handlebars");
+const i18n = require("../../i18n/i18n");
 module.exports = function (tooltipKey) {
-    var safeText = Handlebars.Utils.escapeExpression(i18n.localize(tooltipKey));
+    const safeText = Handlebars.Utils.escapeExpression(i18n.localize(tooltipKey));
     return new Handlebars.SafeString('data-toggle="tooltip" data-placement="top" data-tooltip-class="tt" title="' + safeText + '"');
 };
