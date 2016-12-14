fb0adfa5bb09e994fe727830cf828753f7bd23bf
Ben Nelson <Ben.Nelson@inin.com>
2016-12-14 17:35:52 -0500

    [EMAIL-59] Add localization for the link tooltip strings

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,6 +1,35 @@
 "use strict";
 
 const englishDictionary = require("./strings/en-us.json");
+const _once = require("lodash/once");
+
+
+//---------------------------
+// Localization style element
+//---------------------------
+/*
+Quill has a number of strings hardcoded in CSS: currently the only way to translate those is
+via CSS overrides.  So we insert an element in the head and update it whenever the locale changes
+*/
+const getTranslationStyleElement = _once(() => {
+    const translationStyleElement = document.createElement("style");
+    //class just for information purposes
+    translationStyleElement.class="quilljs-translations";
+    document.head.appendChild(translationStyleElement);
+
+    return translationStyleElement;
+});
+
+function updateTranslationStyleElement () {
+    const translationStyleElement = getTranslationStyleElement();
+    translationStyleElement.innerText = `
+.ql-snow .ql-tooltip[data-mode="link"]::before      { content: "${i18n.localize("tooltip.link.label")}"; }
+.ql-snow .ql-tooltip            a.ql-action::after  { content: "${i18n.localize("tooltip.edit")}"; }
+.ql-snow .ql-tooltip            a.ql-remove::before { content: "${i18n.localize("tooltip.remove")}"; }
+.ql-snow .ql-tooltip.ql-editing a.ql-action::after  { content: "${i18n.localize("tooltip.save")}"; }
+.ql-snow .ql-tooltip::before                        { content: "${i18n.localize("tooltip.link.visit")}"; }
+    `;
+}
 
 let dictionary = englishDictionary;
 
@@ -11,6 +40,7 @@ const i18n = {
         } catch (e) {
             dictionary = englishDictionary;
         }
+        updateTranslationStyleElement();
     },
     localize: function (key) {
         if(key in dictionary) {
